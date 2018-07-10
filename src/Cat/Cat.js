35b67c5c2736fb190c0e5fce64abc5b23e534f9b
Amélie Benoit <a.benoit@lectra.com>
2018-07-10 15:56:23 +0200

    :ok_hand: Correction of tp1

--- src/Cat/Cat.js
+++ src/Cat/Cat.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Cat.css';
 
-const propTypes = {};
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  meowsPerHour: PropTypes.number
+};
 
-const defaultProps = {};
+const defaultProps = {
+  meowsPerHour: 10
+};
 
 class Cat extends Component {
   render() {
     return (
       <div className="Cat">
-        Hello, my name is Kitty ! I am 3 years old, and I do 11 meows per hour.
+        <p>
+          I am <span className="name">{this.props.name}</span> ! I am
+          {this.props.age} years old and I do {this.props.meowsPerHour} meows
+          per hour
+        </p>
       </div>
     );
   }
