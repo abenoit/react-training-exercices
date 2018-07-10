72bf267d043f06b456899c17ab2751448e461f62
Amélie <amelie.benoit33@gmail.com>
2018-07-10 16:41:50 +0200

    :lipstick: Change style

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { PageHeader } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React exercices</h1>
-        </header>
+        <PageHeader>
+          React Training <small>stage 1</small>
+        </PageHeader>
+
+        <span className="todo">TODO : Give me some cats here ! </span>
       </div>
     );
   }
