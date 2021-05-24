5ad9804967a194aecc0e90e7f483bf4be0f816f1
Amelie Benoit <amelie.benoit@busbud.com>
2021-05-24 16:12:55 +0200

    fix: styles and merge

--- src/Cat/Cat.js
+++ src/Cat/Cat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Proptypes from 'prop-types';
 import './Cat.css';
 
@@ -8,17 +8,15 @@ const propTypes = {};
 // TODO : add a default value for meowsPerHour
 const defaultProps = {};
 
-class Cat extends Component {
-  render() {
-    // TODO : use this.props to access cat's name, age and meowPerHour
-    // Tip : use {} to inject Javascript in the pseudo-HTML below
-    return (
-      <div className="Cat">
-        Hello, my name is Kitty ! I am 3 years old, and I do 11 meows per hour.
-      </div>
-    );
-  }
-}
+const Cat = () => {
+  // TODO : use this.props to access cat's name, age and meowPerHour
+  // Tip : use {} to inject Javascript in the pseudo-HTML below
+  return (
+    <div className="Cat">
+      Hello, my name is Kitty ! I am 3 years old, and I do 11 meows per hour.
+    </div>
+  );
+};
 
 Cat.propTypes = propTypes;
 Cat.defaultProps = defaultProps;
